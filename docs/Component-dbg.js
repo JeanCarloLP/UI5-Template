b66fabe62a7ed611c4bb544b15086292f99f075c
Jean Carlo Leon <jeancarloleonpr@gmail.com>
2021-11-26 15:38:02 +0100

    20211126 JCL docs update

--- docs/Component-dbg.js
+++ docs/Component-dbg.js
@@ -1,43 +1,38 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"./controller/HelloDialog"
+"use strict";
 
-], function (UIComponent, JSONModel, HelloDialog) {
-	"use strict";
-	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-		metadata : {
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-			manifest : "json"
-		},
-		init : function () {
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
-			// set data model
-			var oData = {
-				recipient : {
-					name : "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.setModel(oModel);
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel", "./controller/HelloDialog"], function (UIComponent, JSONModel, HelloDialog) {
+  "use strict";
 
-			// set dialog, we wait for the
-			this.rootControlLoaded().then(function() {
-				this._helloDialog = new HelloDialog(this.getRootControl());
-			}.bind(this));
-		},
+  return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+    metadata: {
+      interfaces: ["sap.ui.core.IAsyncContentCreation"],
+      manifest: "json"
+    },
+    init: function init() {
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments); // set data model
 
+      var oData = {
+        recipient: {
+          name: "World"
+        }
+      };
+      var oModel = new JSONModel(oData);
+      this.setModel(oModel); // set dialog, we wait for the
 
-		exit : function() {
-			if (this._helloDialog) {
-				this._helloDialog.destroy();
-				delete this._helloDialog;
-			}
-		},
+      this.rootControlLoaded().then(function () {
+        this._helloDialog = new HelloDialog(this.getRootControl());
+      }.bind(this));
+    },
+    exit: function exit() {
+      if (this._helloDialog) {
+        this._helloDialog.destroy();
 
-		openHelloDialog : function () {
-			this._helloDialog.open();
-		}
-	});
+        delete this._helloDialog;
+      }
+    },
+    openHelloDialog: function openHelloDialog() {
+      this._helloDialog.open();
+    }
+  });
 });
